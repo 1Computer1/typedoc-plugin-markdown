f88948cb06f355b90e8c088f774f62b3c83cc5d6
1computer1 <onecomputer00@gmail.com>
2020-07-04 05:32:25 -0400

    lowercase flags

--- dist/resources/helpers/member-title.js
+++ dist/resources/helpers/member-title.js
@@ -4,7 +4,7 @@ exports.memberTitle = void 0;
 function memberTitle() {
     const md = [];
     if (this.flags) {
-        md.push(this.flags.join(' '));
+        md.push(this.flags.map(x => x.toLowerCase()).join(' '));
     }
     md.push(this.name);
     return md.join(' ');
--- src/resources/helpers/member-title.ts
+++ src/resources/helpers/member-title.ts
@@ -4,7 +4,7 @@ export function memberTitle(this: DeclarationReflection) {
   const md = [];
 
   if (this.flags) {
-    md.push(this.flags.join(' '));
+    md.push(this.flags.map(x => x.toLowerCase()).join(' '));
   }
   md.push(this.name);
   return md.join(' ');
